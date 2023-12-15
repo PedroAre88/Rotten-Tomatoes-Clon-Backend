2a1ad055221f03c2eeb922f798c07938c7cfb20a
PedroAre88 <106494143+PedroAre88@users.noreply.github.com>
2023-12-15 18:20:40 -0400

    Features
    Añadida la ruta para filtrar las peliculas por genero y calificacion de criticos

---
+++ movie.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const movieController = require('../controllers/movieController');
+
+router.use(auth);
+
+// Ruta para obtener las películas
+router.get('/movies', movieController.getMovies);
+
+// Ruta para obtener una película
+router.get('/movies/:id', movieController.getMovie);
+
+// Ruta para obtener las películas recomendadas
+router.get('/movies/:id/recommendations', movieController.getRecommendedMovies);
+
+// Ruta para buscar películas
+router.get('/movies/search', movieController.searchMovies);
+
+module.exports = router;
